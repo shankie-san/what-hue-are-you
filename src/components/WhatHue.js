6d99cd9be40aebc23c0ca7e90bfa5630951d048e
TheHeat <marc@properdesign.co.uk>
2019-11-05 22:26:08 +0000

    Stylish

--- src/components/WhatHue.js
+++ src/components/WhatHue.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { firestore } from "../fire";
-import Select from 'react-select';
-import { detect } from 'detect-browser';
+import Select from "react-select";
+import { detect } from "detect-browser";
 
 const WhatHue = () => {
-
   const [content, setContent] = useState({});
   const [selectedOption, setSelectedOption] = useState("");
 
@@ -25,32 +24,41 @@ const WhatHue = () => {
   const handleSubmit = e => {
     firestore
       .collection("responses")
-      .add({colour: selectedOption.value, browser: detect().name})
-      .catch(function (error) {
+      .add({ colour: selectedOption.value, browser: detect().name })
+      .catch(function(error) {
         console.error(error);
       })
-      .then((ref) => {
+      .then(ref => {
         setSelectedOption("");
-        alert("Thanks for sharing your personal data with us 😘")
-      })
+        alert("Thanks for sharing your sensitive personal data with us 😘");
+      });
     e.preventDefault();
-  }
+  };
 
   return (
-    <div>
-      <h1>{content.title}</h1>
-      <div>
-        <p>{content.question}</p>
-        {content.colours &&
-          <form onSubmit={handleSubmit}>
+    <div className={`whatHue ${selectedOption.value}`}>
+      <div className="whatHue-inner">
+        <header className="whatHue-header">
+          <h1>{content.title}</h1>
+        </header>
+        {content.colours && (
+          <form className="whatHue-form" onSubmit={handleSubmit}>
+            <label className="whatHue-label" htmlFor="colorSelect">
+              {content.question}
+            </label>
             <Select
+              className="whatHue-select"
+              name="colorSelect"
               value={selectedOption}
-              onChange={(selectedOption) => setSelectedOption(selectedOption)}
+              onChange={selectedOption => setSelectedOption(selectedOption)}
               options={content.colours}
             />
-            <input type="submit" value="Submit"/>
+            <input className="whatHue-submit" type="submit" value="Submit" />
           </form>
-        }
+        )}
+        <footer className="whatHue-footer">
+          a <a href="https://properdesign.co.uk">proper</a> silly thing
+        </footer>
       </div>
     </div>
   );
